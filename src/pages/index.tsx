747b2265e35f92cc9f99e93117e25dc2f1cee4c3
lucianodiisouza <lucianodiisouza@hotmail.com>
2021-07-01 01:20:09 -0300

    home page pt II

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ButtonIcon } from "../components";
+import homeImage from "../../public/homeimage.svg";
+
 import styles from "../styles/pages/home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.homeContainer}>
       <div
-        className={`row d-flex flex-column-reverse align-items-center justify-content-center flex-lg-row card-base border-radius-20 ${styles.homeText}`}
+        className={`row d-flex flex-column-reverse align-items-center justify-content-center flex-lg-row card-base border-radius-20 ${styles.homeContent}`}
       >
         <div
           className={`col-lg-6 d-flex flex-column align-items-center mt-4 mt-md-0 ${styles.homeText}`}
@@ -16,6 +21,16 @@ export default function Home() {
             Ajudaremos você a encontrar os melhores produtos disponíveis no
             mercado.
           </p>
+          <Link href="/catalog">
+            <a>
+              <ButtonIcon label="inicie agora a sua busca" />
+            </a>
+          </Link>
+        </div>
+        <div
+          className={`col-lg-6 d-flex align-items-center justify-content-center align-items-lg-center justify-content-lg-start ${styles.homeImgContainer}`}
+        >
+          <Image src={homeImage} alt="Home Page Image" />
         </div>
       </div>
     </div>
